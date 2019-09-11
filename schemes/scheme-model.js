dbbbe170e192a6745819df55e2cd394b015c1906
lilyhoratio <lilyhzhou@gmail.com>
2019-09-11 14:26:10 -0700

    findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,8 +1,9 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-  find
-  //   findById,
+  find,
+  findById,
+  findSteps
   //   add,
   //   update,
   //   remove
@@ -19,19 +20,41 @@ function find() {
 //   - Expects a scheme `id` as its only parameter.
 //   - Resolve to a single scheme object.
 //   - On an invalid `id`, resolves to `null`.
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
 
 // - `findSteps(id)`:
 //   - Expects a scheme `id`.
 //   - Resolves to an array of all correctly ordered step for the given scheme: `[ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ]`.
 //   - This array should include the `scheme_name` _not_ the `scheme_id`.
+function findSteps(id) {
+  return (
+    db("steps")
+      .innerJoin("schemes", "steps.scheme_id", "=", "schemes.id")
+      .select(
+        "steps.id",
+        "schemes.scheme_name",
+        "steps.step_number",
+        "steps.instructions"
+      )
+      // .where({ scheme_id: id });
+      .where("scheme_id", id)
+  );
+}
+
 // - `add(scheme)`:
 //   - Expects a scheme object.
 //   - Inserts scheme into the database.
 //   - Resolves to the newly inserted scheme, including `id`.
+
 // - `update(changes, id)`:
 //   - Expects a changes object and an `id`.
 //   - Updates the scheme with the given id.
 //   - Resolves to the newly updated scheme object.
+
 // - `remove(id)`:
 //   - Removes the scheme object with the provided id.
 //   - Resolves to the removed scheme
